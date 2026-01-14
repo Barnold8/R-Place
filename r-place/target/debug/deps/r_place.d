8a083956a5e2ff2eee3a337f7d4f67195755d772
Barnold88 <bandslover@outlook.com>
2026-01-14 01:45:06 +0000

    Add boilerplate project

---
+++ r-place/target/debug/deps/r_place.d
@@ -0,0 +1,5 @@
+D:\Code\R-Place\r-place\target\debug\deps\r_place.d: src\main.rs
+
+D:\Code\R-Place\r-place\target\debug\deps\r_place.exe: src\main.rs
+
+src\main.rs:
---
+++ r-place/target/debug/r-place.d
@@ -0,0 +1,1 @@
+D:\Code\R-Place\r-place\target\debug\r-place.exe: D:\Code\R-Place\r-place\src\main.rs
